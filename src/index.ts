b3b82d279c74919c3bc86752220b763907851adf
prasanth8961 <prasanthprs143@gmail.com>
2025-08-19 11:06:11 +0530

    Update: cors origin

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 const ENVIRONMENT = process.env.NODE_ENV || 3000;
 
 const corseOptions = {
-  origin: "https://eventsphere-admin-panel.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true
+  origin: "*"
 };
 app.use(express.static("public"));
 
